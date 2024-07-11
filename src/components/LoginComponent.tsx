d9c0ccefade4eb23c5883aa44eba93e484e6d137
Misha McFeat <misha.mcfeat@gmail.com>
2024-07-11 16:14:40 +0100

    Login Component updates

--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
-import { useAddUserMutation } from '../store';
+import { useAddUserMutation, useLoginUserMutation } from '../store';
 
 const LoginComponent: React.FC = () => {
     const [addUser, { isLoading, isError, isSuccess }] = useAddUserMutation();
+    const [loginUser, { loginisLoading, loginisError, loginisSuccess }] = useLoginUserMutation();
     const [name, setName] = useState<string>('');
     const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
 
     const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setName(e.target.value);
@@ -14,40 +17,76 @@ const LoginComponent: React.FC = () => {
         setEmail(e.target.value);
     };
 
-    const handleSubmit = async (e: React.FormEvent) => {
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSignup = async (e: React.FormEvent) => {
         e.preventDefault();
-        await addUser({ name, email });
+        // needs to be encrypted client side?
+        await addUser({ name, email, password });
         setName('');
         setEmail('');
+        setPassword('');
     };
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Input Name</label>
-                    <input
-                        type="text"
-                        value={name}
-                        onChange={handleNameChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Input Email</label>
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={handleEmailChange}
-                        required
-                    />
-                </div>
-                <button type="submit" disabled={isLoading}>
-                    {isLoading ? 'Adding User...' : 'Add User'}
-                </button>
-            </form>
-            {isError && <div>Error adding user</div>}
-            {isSuccess && <div>User added successfully!</div>}
+            <div>
+                <form onSubmit={handleSignup}>
+                    <div>
+                        <label>Input Name</label>
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={handleNameChange}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label>Input Email</label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={handleEmailChange}
+                            required
+                        />
+                    </div>
+                    <button type="submit" disabled={isLoading}>
+                        {isLoading ? 'Adding User...' : 'Add User'}
+                    </button>
+                </form>
+                {isError && <div>Error adding user</div>}
+                {isSuccess && <div>User added successfully!</div>}
+            </div>
+            <div>
+                <form onSubmit={handleLogin}>
+                    <div>
+                        <label>Input Email</label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={handleEmailChange}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label>Input Password</label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                            required
+                        />
+                    </div>
+                    <button type="submit" disabled={isLoading}>
+                        {isLoading ? 'Adding User...' : 'Add User'}
+                    </button>
+                </form>
+                {isError && <div>Error adding user</div>}
+                {isSuccess && <div>User added successfully!</div>}
+
+            </div>
         </div>
     );
 };
